a19ad20039b931ff0fd741e9023a14109e79fae8
Rica Gacusan <ricagacusan41@gmail.com>
2026-05-14 23:48:12 +0800

    Updated login template and navbar design

--- init_db.py
+++ init_db.py
@@ -1,9 +1,7 @@
-# init_db.py
 import os
 from app import app
 from models import db
 
-# Ensure the instance folder exists
 if not os.path.exists(app.instance_path):
     os.makedirs(app.instance_path)
 
